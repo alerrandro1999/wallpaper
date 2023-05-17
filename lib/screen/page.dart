008e4544bf5578a28fbc9a6696da424574a91d04
alerrandro borges <alerrandrokaton@gmail.com>
2023-05-17 08:49:22 -0300

    feat: consuming api, e listing api

--- lib/screen/page.dart
+++ lib/screen/page.dart
@@ -1,9 +1,8 @@
-import 'dart:io';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:troca_papel/database/pathImages.dart';
+import 'package:http/http.dart' as http;
+import 'package:troca_papel/key/api_key.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -14,37 +13,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<XFile> selectedImages = [];
-
-  List<File> savedImages = [];
+  final List<String> _imageUrls = [];
+  bool _isLoading = false;
+  int _page = 1;
+  final int _perPage = 10;
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    loadSavedImages();
+    _scrollController.addListener(_scrollListener);
+    _fetchMoreImages();
   }
 
-  void openGallery() async {
-    List<XFile>? images = await ImagePicker().pickMultiImage();
-
-    setState(() {
-      selectedImages = images;
-    });
-
-    await PathImages().saveImage(selectedImages);
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 
-  Future<void> loadSavedImages() async {
-    Directory directory = await getApplicationDocumentsDirectory();
-    String folderPath = '${directory.path}/imagens';
+  void _scrollListener() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      _fetchMoreImages();
+    }
+  }
 
-    Directory folder = Directory(folderPath);
-    List<File> images =
-        await folder.list().map((file) => File(file.path)).toList();
+  Future<void> _fetchMoreImages() async {
+    if (!_isLoading) {
+      setState(() {
+        _isLoading = true;
+      });
 
-    setState(() {
-      savedImages = images;
-    });
+      final String url =
+          'https://api.pexels.com/v1/search?query=anime cosplay&page=$_page&per_page=$_perPage';
+
+      final response = await http.get(Uri.parse(url), headers: {
+        'Authorization': key,
+      });
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+        final List<dynamic> photos = data['photos'];
+
+        List<String> imageUrls = [];
+        for (var photo in photos) {
+          final String imageUrl = photo['src']['portrait'];
+          imageUrls.add(imageUrl);
+        }
+
+        setState(() {
+          _imageUrls.addAll(imageUrls);
+          _isLoading = false;
+          _page++;
+        });
+      } else {
+        throw Exception('Failed to fetch images');
+      }
+    }
+  }
+
+  Widget _buildImageItem(int index) {
+    if (index >= _imageUrls.length) {
+      if (_isLoading) {
+        return const Center(child: CircularProgressIndicator());
+      } else {
+        return const SizedBox.shrink();
+      }
+    }
+
+    return Image.network(_imageUrls[index]);
   }
 
   @override
@@ -55,28 +93,28 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
         backgroundColor: const Color(0xFF0D6847),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-          ),
-          itemCount: savedImages.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Image.file(savedImages[index]);
-          },
-        ),
-      ),
-      floatingActionButton: SizedBox(
-        width: 70,
-        height: 70,
-        child: FloatingActionButton(
-          onPressed: openGallery,
-          tooltip: 'Increment',
-          backgroundColor: const Color(0xFF0D6847),
-          child: const Icon(
-            Icons.add,
-            size: 40,
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification notification) {
+          if (notification is ScrollEndNotification &&
+              _scrollController.position.extentAfter == 0) {
+            _fetchMoreImages();
+          }
+          return false;
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GridView.builder(
+            controller: _scrollController,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisExtent: 180,
+              crossAxisSpacing: 0,
+              mainAxisSpacing: 7,
+            ),
+            itemCount: _imageUrls.length + 1,
+            itemBuilder: (context, index) {
+              return _buildImageItem(index);
+            },
           ),
         ),
       ),
